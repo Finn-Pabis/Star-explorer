b81ef3f6361a65ffabf3c4aa6cf9260f69816c9c
Finn-Pabis <105420133+Finn-Pabis@users.noreply.github.com>
2022-05-26 17:47:12 +0200

    Update script.js

--- Star-explorer/script.js
+++ Star-explorer/script.js
@@ -9,5 +9,4 @@ const tl = new TimelineMax();
 
 tl.
 fromTo(hero,0.85,{height: "0%"}, {height: "85%", ease: Power2.easeInOut})
-.fromTo([headline, aboutLink], 1.1, {delay: 0.2, opacity: 0}, {opacity: 1, ease: Power4.easeInOut})
-.fromTo(hero,1.2, {width: "100%"}, {width: "90%", ease: Power2.easeInOut});
+.fromTo([headline, aboutLink], 1.1, {delay: 0.2, opacity: 0}, {opacity: 1, ease: Power4.easeInOut});
