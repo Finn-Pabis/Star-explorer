39d3519860aced009c4f4bd95b589723d954b3ac
Finn-Pabis <105420133+Finn-Pabis@users.noreply.github.com>
2022-05-27 21:28:25 +0200

    Delete Star-explorer directory

--- Star-explorer/scripts/about-script.js
+++
@@ -1,7 +0,0 @@
-const section1 = document.querySelector(".section1");
-const section2 = document.querySelector(".section2");
-const section3 = document.querySelector(".section3");
-let sections = [section1, section2, section3];
-
-gsap.registerPlugin(ScrollTrigger);
-
